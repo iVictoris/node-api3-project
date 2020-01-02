58060de96906becd17e4ba43b7f7b3df47ecfb56
Victor Tran <victor.n.tran@outlook.com>
2020-01-02 17:41:56 -0500

    add boilerplate code for express application

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require('express')
+const app = express()
+const logger = require('./middleware/logger');
+
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.use(logger)
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(port, () => {
+  console.log(`App is listening to port ${port}`);
+})
