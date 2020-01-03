1a16071d81e8dbfae10888821b28cec12849383e
Victor Tran <victor.n.tran@outlook.com>
2020-01-02 23:37:17 -0500

    add users route and import user route and use it

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const logger = require('./middleware/logger');
+const userRouter = require('./users/userRouter');
 
 const port = process.env.PORT || 3000
 
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use(logger)
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.use('/api/users', userRouter)
 
 app.listen(port, () => {
   console.log(`App is listening to port ${port}`);
